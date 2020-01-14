199e5b035ed53e3173ec605069a1c61c5b70b622
Ben Fox <benfox1216@gmail.com>
2020-01-14 09:20:35 -0700

    Create Vendor class so test_it_exists passes

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -1,15 +1,18 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/item'
-require './lib/item'
+require './lib/vendor'
 
 class ItemTest < Minitest::Test
   def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
     
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
   
   def test_it_exists
-    
+    assert_instance_of Vendor, @vendor
   end
   
   def test_it_has_attributes
