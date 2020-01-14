bc84385c361fc2f027a30a727dc95c5743e26b4c
Ben Fox <benfox1216@gmail.com>
2020-01-14 09:53:00 -0700

    Add add_vendor method to Market class, test passing

--- test/market_test.rb
+++ test/market_test.rb
@@ -9,6 +9,8 @@ class ItemTest < Minitest::Test
     @market = Market.new("South Pearl Street Farmers Market")
     
     @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
     
     @item1 = Item.new({name: "Peach", price: "$0.75"})
     @item2 = Item.new({name: "Tomato", price: '$0.50'})
@@ -23,5 +25,22 @@ class ItemTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "South Pearl Street Farmers Market", @market.name
     assert_equal [], @market.vendors
+  end
+  
+  def test_it_can_add_vendor
+    @market.add_vendor(@vendor1)
+    assert_equal [@vendor1], @market.vendors
+  end
+  
+  def test
+    @vendor1.stock(item1, 35)
+    @vendor1.stock(item2, 7)
+    @vendor2.stock(item4, 50)
+    @vendor2.stock(item3, 25)
+    @vendor3.stock(item1, 65)
+
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
   end
 end
